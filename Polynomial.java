ea2b6126ee39bf8d46efc5d73c33db900d1c3c45
TKCoder13 <tkcoding13@gmail.com>
2023-03-02 13:55:55 -0800

    Horner's Method was added

--- Polynomial.java
+++ Polynomial.java
@@ -15,23 +15,55 @@ public class Polynomial {
     //     return result;
     // }
 
-    public static double evalPoly(double[] coefs, int x) {
+    // public static double evalPoly(double[] coefs, int x) { // This is better than the first one, not the best tho
+    //     add = 0;
+    //     multi = 0;
+    //     double result = 0;
+    //     double xPower = Math.pow(x, coefs.length - 1);
+    //     for (int i = coefs.length - 1; i >= 0; i--) {
+    //         result += (coefs[i] * xPower); ++add; ++multi;
+    //         xPower /= x; ++multi;
+    //     }
+    //     return result;
+    // }
+
+    public static double evalPoly(double[] coefs, int x) { // Same amt of adds and multis, but got rid of Math.pow()
         add = 0;
         multi = 0;
         double result = 0;
-        double xPower = Math.pow(x, coefs.length - 1);
-        for (int i = coefs.length - 1; i >= 0; i--) {
+        double xPower = 1;
+        for (int i = 0; i < coefs.length; i++) {
             result += (coefs[i] * xPower); ++add; ++multi;
-            xPower /= x; ++multi;
+            xPower *= x; ++multi;
         }
         return result;
     }
+
+    public static double hornersMethod(double[] coefs, int x) { // Uses factorization, halves the amount of multiplication
+        //counters
+        add = 0;
+        multi = 0;
+
+        int n = coefs.length;
+        double result = coefs[n-1];
+
+        for (int i = n-2; i >= 0; i--) {
+            result *= x; ++add;
+            result += coefs[i]; ++multi;
+        }
+        return result;
+    }
+
     public static void main(String args[]) {
         double[] arrayOfCoefs = {0,1,2,3,4};
-        double output = evalPoly(arrayOfCoefs, 1);
-        System.out.println(output);
-        System.out.println(add);
-        System.out.println(multi);
-        
+        double output = evalPoly(arrayOfCoefs, 2);
+        System.out.println("Generic Polynomial Formula: " + output);
+        System.out.println("Additions: " + add);
+        System.out.println("Multiplcations: " + multi);
+
+        double hornerOutput = hornersMethod(arrayOfCoefs, 2);
+        System.out.println("Horner's Method: " + hornerOutput);
+        System.out.println("Additions: " + add);
+        System.out.println("Multiplcations: " + multi);
     }
 }
